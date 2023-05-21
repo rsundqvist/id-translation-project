84b2765abb9e1d8a66e7de7a33b1a7b34a2bbddd
Richard Sundqvist <richard.sundqvist@live.se>
2023-05-21 12:59:11 +0200

    Update {{cookiecutter.namespace}}.id_translation.translate()
    - Expose override_function (previously always None)
    - Expose fmt (new in 0.4.0)
    - Add optional copy-kwargs to translate with temporary modifications to the singleton

--- demo/ute-id-translation/tests/test_demo_some_things.py
+++ demo/ute-id-translation/tests/test_demo_some_things.py
@@ -4,12 +4,21 @@ from ute import id_translation
 
 
 def test_fancy_format_string():
-    translator = id_translation.create_translator().copy(fmt="{name} <{email!r}> {last_name}")
-    print("\nSome more information about the first ten customers:")
-    pprint(translator.translate(list(range(10)), names="customer_id"))
+    print("\nSome more information about the first three:")
+    pprint(id_translation.translate(
+        [1, 2, 3],
+        names="customer_id",
+        fmt="{name} <{email!r}> {last_name}",
+
+    ))
 
 
 def test_untranslatable_ids():
-    translator = id_translation.create_translator()
     print("\nSome untranslatable IDs:")
-    pprint(translator.translate([0, -1, 10000], names="customer_id"))
+    pprint(
+        id_translation.translate(
+            [0, -1, 10000],
+            names="customer_id",
+            maximal_untranslated_fraction=1,
+        )
+    )
